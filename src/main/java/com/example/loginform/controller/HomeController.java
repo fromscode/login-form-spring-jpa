add7c3549897c300b271a98b43784d9d2a3ed609
fromscode <chakrabortyshubham00@gmail.com>
2025-03-24 11:56:51 +0000

    feat: add mapping for homepage links

--- src/main/java/com/example/loginform/controller/HomeController.java
+++ src/main/java/com/example/loginform/controller/HomeController.java
@@ -1,13 +1,24 @@
 package com.example.loginform.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @Controller
 public class HomeController {
 	
-	@RequestMapping("/")
+	@GetMapping("/")
 	public String homePage() {
 		return "index";
+	}
+	
+	@GetMapping("signup")
+	public String signUpPage() {
+		return "signup";
+	}
+	
+	@GetMapping("forgotPassword")
+	public String forgotPasswordPage() {
+		return "forgotPassword";
 	}
 }
